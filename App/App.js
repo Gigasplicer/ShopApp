0c0143a3224a93f0bdf6b8ae872cfd46112a3fa6
Gigasplicer <andy.mccormick10@gmail.com>
2020-08-04 11:55:06 -0500

    actually committing the subdirectories this time, Maybe

---
+++ App/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>New app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
+//npm install --save redux react-redux react-navigation react-navigation-header-buttons
---
+++ App/screens/shop/OrdersScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+const OrdersScreen = props =>{
+
+};
+const styles = StyleSheet.create({
+
+});
+
+export default OrdersScreen;
