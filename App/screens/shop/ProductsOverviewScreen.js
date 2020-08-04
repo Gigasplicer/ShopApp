0dbfff01db80ec49df8f2d8a811a2fd67a494a31
Gigasplicer <andy.mccormick10@gmail.com>
2020-08-04 12:21:34 -0500

    uploading to grab dummy-data

--- App/screens/shop/ProductsOverviewScreen.js
+++ App/screens/shop/ProductsOverviewScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
+import { View, Text, StyleSheet, FlatList} from 'react-native';
 
 const ProductsOverviewScreen = props =>{
+ return(
+     <FlatList/>
+ )
 
 };
+
 const styles = StyleSheet.create({
 
 });
---
+++ App/store/reducers/products.js
@@ -0,0 +1,7 @@
+const initialStat = {
+    availableProducts: [],//
+    userProducts: [],
+}
+export default(state = initialState, action) => {// we get an action? not sure
+    return state;
+}
